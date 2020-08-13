db5e8e817c43d2eccbbe0c58dc87a5145bac12b0
Ghosalkar <vghosalkar@deloitte.com>
2020-08-13 11:46:36 -0400

    Auth Repository Update

--- MultiMonth.API/Data/DataContext.cs
+++ MultiMonth.API/Data/DataContext.cs
@@ -8,7 +8,8 @@ namespace MultiMonth.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
 
         public DbSet<Value> Values { get; set; }
-    
+        public DbSet<User> Users { get; set; }
+            
     }
 
 }
--- MultiMonth.API/Migrations/20200813142259_AddedUserEntity.Designer.cs
+++ MultiMonth.API/Migrations/20200813142259_AddedUserEntity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MultiMonth.API.Data;
 
 namespace MultiMonth.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200813142259_AddedUserEntity")]
+    partial class AddedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
